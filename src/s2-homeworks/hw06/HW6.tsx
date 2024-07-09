5e0b8fe374dd0f53fdf12f0d23f8acb27d67c2b2
Verminas <veremeeva96@gmail.com>
2024-07-09 15:55:00 +0300

    feat: add hw 06 for sprint 03

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -6,8 +6,8 @@ import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
 /*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
- * 2 - дописать логику функции restore
+ * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack +
+ * 2 - дописать логику функции restore +
  * 3 - сделать стили в соответствии с дизайном
  */
 
@@ -19,11 +19,11 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        setValue(restoreState<string>('hw6-editable-span-value', value))
     }
 
     return (
-        <div id={'hw6'}>
+        <div id={'hw6'} className={s2.hw6}>
             <div className={s2.hwTitle}>Homework #6</div>
 
             {/*демонстрация возможностей компоненты:*/}
