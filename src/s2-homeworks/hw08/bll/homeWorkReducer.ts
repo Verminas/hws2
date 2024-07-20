b8100476ffcacf93d2849ac4e8fe40c9e3d0a151
Verminas <veremeeva96@gmail.com>
2024-07-20 16:36:15 +0300

    feat: add hw 08 for sprint 03

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            switch (action.payload) {
+                case 'up': {
+                    return [...state].sort((a, b) => a.name.localeCompare(b.name))
+                }
+                case 'down': {
+                    return [...state].sort((a, b) => b.name.localeCompare(a.name))
+                }
+                default:
+                    return state;
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            const {payload} = action
+            return state.filter(h => h.age >= payload) // need to fix
         }
         default:
             return state
