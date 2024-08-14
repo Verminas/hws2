3b256f73912f83f0c3db7f3fdb705d3e7be3c9d6
Verminas <veremeeva96@gmail.com>
2024-08-14 14:10:03 +0300

    feat: add hw 12 for sprint 04

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
+import {homeWorkReducer} from "../../hw08/bll/homeWorkReducer";
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type InitialStateType = typeof initState
+
+export const themeReducer = (state: InitialStateType = initState, action: ActionsType): InitialStateType => { // fix any
     switch (action.type) {
         // дописать
+
+        case "SET_THEME_ID": {
+            return {...state, themeId: action.id}
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) // fix any
+
+type ActionsType = ReturnType<typeof changeThemeId>
