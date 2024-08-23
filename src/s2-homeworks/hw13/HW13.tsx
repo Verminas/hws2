1ffb93fef441f39990cef2ba16e7d19027448e88
Verminas <veremeeva96@gmail.com>
2024-08-23 19:04:11 +0300

    feat: add hw13 for spring 05(att 01)

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -14,63 +14,190 @@ import errorUnknown from './images/error.svg'
 * 3 - сделать стили в соответствии с дизайном +
 * */
 
+// const HW13 = () => {
+//   const [code, setCode] = useState('')
+//   const [text, setText] = useState('')
+//   const [info, setInfo] = useState('')
+//   const [image, setImage] = useState('')
+//   // const [disabled, setDisabled] = useState(null)
+//
+//   const loading = '...loading'
+//
+//   const changeCommonState = (info: string = loading, code: string = '', image: string = '', text: string = '') => {
+//     setInfo(info)
+//     setCode(code)
+//     setImage(image)
+//     setText(text)
+//   }
+//
+//   const send = (x?: boolean | null) => () => {
+//     const url =
+//       x === null
+//         ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
+//         : 'https://samurai.it-incubator.io/api/3.0/homework/test'
+//
+//     changeCommonState(`${x}`)
+//
+//     // setInfo(info)
+//     // setCode(code)
+//     // setImage(image)
+//     // setText(text)
+//
+//
+//     // setDisabled(true)
+//     axios
+//       .post<{ errorText: string, info: string }>(url, {success: x})
+//       .then((res) => {
+//         changeCommonState(res.data.info, `Код ${res.status}!`, success200, res.data.errorText)
+//         // setDisabled(false)
+//
+//       })
+//       .catch((e) => {
+//         if (e.response?.status > 0) {
+//           if (e.response?.status >= 400 && e.response?.status < 500) {
+//             changeCommonState(e.response?.data?.info, `Ошибка ${e.response.status}!`, error400, e.response?.data?.errorText)
+//             // setDisabled(false)
+//           } else {
+//             changeCommonState(e.response?.data?.info, `Ошибка ${e.response.status}!`, error500, e.response?.data?.errorText)
+//             // setDisabled(false)
+//           }
+//
+//         } else {
+//           changeCommonState(e.name, 'Error!', errorUnknown, e.message)
+//           // setDisabled(false)
+//         }
+//       })
+//   }
+//
+//   return (
+//     <div id={'hw13'} className={s2.hws}>
+//       <div className={s2.hwTitle}>Homework #13</div>
+//
+//       <div className={s2.hw}>
+//         <div className={s.buttonsContainer}>
+//           <SuperButton
+//             id={'hw13-send-true'}
+//             onClick={send(true)}
+//             xType={'secondary'}
+//             // дописать
+//             disabled={info === 'true'}
+//
+//           >
+//             Send true
+//           </SuperButton>
+//           <SuperButton
+//             id={'hw13-send-false'}
+//             onClick={send(false)}
+//             xType={'secondary'}
+//             //
+//             disabled={info === loading}
+//
+//           >
+//             Send false
+//           </SuperButton>
+//           <SuperButton
+//             id={'hw13-send-undefined'}
+//             onClick={send(undefined)}
+//             xType={'secondary'}
+//             // дописать
+//             disabled={info === loading}
+//
+//           >
+//             Send undefined
+//           </SuperButton>
+//           <SuperButton
+//             id={'hw13-send-null'}
+//             onClick={send(null)} // имитация запроса на не корректный адрес
+//             xType={'secondary'}
+//             // дописать
+//             disabled={info === loading}
+//
+//           >
+//             Send null
+//           </SuperButton>
+//         </div>
+//
+//         <div className={s.responseContainer}>
+//           <div className={s.imageContainer}>
+//             {image && <img src={image} className={s.image} alt="status"/>}
+//           </div>
+//
+//           <div className={s.textContainer}>
+//             <div id={'hw13-code'} className={s.code}>
+//               {code}
+//             </div>
+//             <div id={'hw13-text'} className={s.text}>
+//               {text}
+//             </div>
+//             <div id={'hw13-info'} className={s.info}>
+//               {code && info}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+
 const HW13 = () => {
   const [code, setCode] = useState('')
   const [text, setText] = useState('')
   const [info, setInfo] = useState('')
   const [image, setImage] = useState('')
-  // const [disabled, setDisabled] = useState(null)
-
-  const loading = '...loading'
-
-  const changeCommonState = (info: string = loading, code: string = '', image: string = '', text: string = '') => {
-    setInfo(info)
-    setCode(code)
-    setImage(image)
-    setText(text)
-  }
+  const [dis, setDis] = useState(false)
 
   const send = (x?: boolean | null) => () => {
+    setDis(true)
     const url =
       x === null
         ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
         : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
-    changeCommonState(`${x}`)
+    setCode('')
+    setImage(errorUnknown)
 
-    // setInfo(info)
-    // setCode(code)
-    // setImage(image)
-    // setText(text)
+    setInfo('...loading')
 
-
-    // setDisabled(true)
     axios
-      .post<{ errorText: string, info: string }>(url, {success: x})
+      .post(url, {success: x})
       .then((res) => {
-        changeCommonState(res.data.info, `Код ${res.status}!`, success200, res.data.errorText)
-        // setDisabled(false)
+        setText(res.data.errorText)
+        setCode(`Код ${res.status} !!!!`)
+        setImage(success200)
+        setInfo(res.data.info)
+
 
       })
+
       .catch((e) => {
-        if (e.response?.status > 0) {
-          if (e.response?.status >= 400 && e.response?.status < 500) {
-            changeCommonState(e.response?.data?.info, `Ошибка ${e.response.status}!`, error400, e.response?.data?.errorText)
-            // setDisabled(false)
-          } else {
-            changeCommonState(e.response?.data?.info, `Ошибка ${e.response.status}!`, error500, e.response?.data?.errorText)
-            // setDisabled(false)
-          }
 
+        if (e.response.data) {
+          setText(e.response.data.errorText)
+          setInfo(e.response.data.info)
+
+          if (e.response.status === 400) {
+            setCode(e.response.status)
+            setImage(error400)
+          }
+          if (e.response.status === 500) {
+            setCode(e.response.status)
+            setImage(error500)
+          }
         } else {
-          changeCommonState(e.name, 'Error!', errorUnknown, e.message)
-          // setDisabled(false)
+
+          setText(e.message)
+          setInfo(e.name)
         }
-      })
+
+      }).finally(() => {
+      setDis(false)
+    })
+
   }
 
   return (
-    <div id={'hw13'} className={s2.hws}>
+    <div id={'hw13'}>
       <div className={s2.hwTitle}>Homework #13</div>
 
       <div className={s2.hw}>
@@ -79,8 +206,7 @@ const HW13 = () => {
             id={'hw13-send-true'}
             onClick={send(true)}
             xType={'secondary'}
-            // дописать
-            disabled={info === 'true'}
+            disabled={dis}
 
           >
             Send true
@@ -89,18 +215,19 @@ const HW13 = () => {
             id={'hw13-send-false'}
             onClick={send(false)}
             xType={'secondary'}
-            //
-            disabled={info === loading}
+            disabled={dis}
+
 
           >
             Send false
           </SuperButton>
+
           <SuperButton
             id={'hw13-send-undefined'}
             onClick={send(undefined)}
             xType={'secondary'}
-            // дописать
-            disabled={info === loading}
+            disabled={dis}
+
 
           >
             Send undefined
@@ -109,8 +236,8 @@ const HW13 = () => {
             id={'hw13-send-null'}
             onClick={send(null)} // имитация запроса на не корректный адрес
             xType={'secondary'}
-            // дописать
-            disabled={info === loading}
+            disabled={dis}
+
 
           >
             Send null
@@ -130,7 +257,7 @@ const HW13 = () => {
               {text}
             </div>
             <div id={'hw13-info'} className={s.info}>
-              {code && info}
+              {info}
             </div>
           </div>
         </div>
@@ -138,5 +265,6 @@ const HW13 = () => {
     </div>
   )
 }
+
 
 export default HW13
